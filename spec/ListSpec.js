98c2e8e02979d1c674fb6afb28c844e3bcec4f36
niklas.leopold <leopold.niklas@gmail.com>
2015-09-23 20:45:45 +0200

    added a List

--- spec/ListSpec.js
+++ spec/ListSpec.js
@@ -115,8 +115,170 @@ define(['List'], function (List) {
             unitUnderTest.append("a");
             expect(unitUnderTest.head.next.next.next).toEqual(undefined);
         }); 
-         
         
+        it("head after delete of undefined in [] is undefined", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head);
+            expect(unitUnderTest.head).toEqual(undefined);
+        });         
+         
+        it("head after delete of a in [a] is undefined", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head);
+            expect(unitUnderTest.head).toEqual(undefined);
+        });  
+        
+        it("head after delete of a [a,b] is b", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head);
+            expect(unitUnderTest.head.content).toEqual("b");
+        });  
+
+        it("head.next after delete of a [a,b] is b", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head);
+            expect(unitUnderTest.head.next).toEqual(undefined);
+        }); 
+        
+        it("head.prev after delete of a [a,b] is b", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head);
+            expect(unitUnderTest.head.prev).toEqual(undefined);
+        });          
+        
+        
+        it("head after delete of b [a,b] is a", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head.next);
+            expect(unitUnderTest.head.content).toEqual("a");
+        });        
+        
+        it("head after delete of b [a,b] is b", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head);
+            expect(unitUnderTest.head.content).toEqual("b");
+        });
+        
+        it("head after delete of b in [a,b] is a", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head.next);
+            expect(unitUnderTest.head.content).toEqual("a");
+        });  
+        
+        it("head.next after delete of b in [a,b] is undefined", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head.next);
+            expect(unitUnderTest.head.next).toEqual(undefined);
+        });         
+        
+        it("head.next after delete of b in [a,b,c] is c", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("c");
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head.next);
+            expect(unitUnderTest.head.next.content).toEqual("c");
+        });    
+        
+        it("head.next.prev after delete of b in [a,b,c] is a", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("c");
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head.next);
+            expect(unitUnderTest.head.next.prev.content).toEqual("a");
+        });   
+        
+        it("iterate over [a,b,c,d] results in [a,b,c,d]", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("d");
+            unitUnderTest.append("c");
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            var current = unitUnderTest.head;
+            var result = [];
+            result.push(current.content);
+            while(current.next){
+                result.push(current.next.content);
+                current = current.next;
+            }
+            expect(result).toEqual(["a","b","c","d"]);
+        });  
+        
+        it("iterate over [a,b,c,d] when c has been deleted results in [a,b,d]", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("d");
+            unitUnderTest.append("c");
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            unitUnderTest.delete(unitUnderTest.head.next.next);
+            var current = unitUnderTest.head;
+            var result = [];
+            result.push(current.content);
+            while(current.next){
+                result.push(current.next.content);
+                current = current.next;
+            }
+            expect(result).toEqual(["a","b","d"]);
+        });     
+        
+       it("iterate over [a,b,c,d] results in [a,b,c,d]", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("d");
+            unitUnderTest.append("c");
+            unitUnderTest.append("b");
+            unitUnderTest.append("a");
+            var iterator = unitUnderTest.iterator();
+            var result = [];
+            while(iterator.hasNext()){
+                result.push(iterator.next().content);
+            }
+            expect(result).toEqual(["a","b","c","d"]);
+            
+            var newIterator = unitUnderTest.iterator();
+            var newResult = [];
+            while(newIterator.hasNext()){
+                newResult.push(newIterator.next().content);
+            }
+            expect(newResult).toEqual(["a","b","c","d"]);            
+        });  
+        
+       it("iterate over [] results in []", function () {
+            var unitUnderTest = new List();
+            var iterator = unitUnderTest.iterator();
+            var result = [];
+            while(iterator.hasNext()){
+                result.push(iterator.next().content);
+            }
+            expect(result).toEqual([]);
+        });  
+        
+       it("iterate over [a] results in [a]", function () {
+            var unitUnderTest = new List();
+            unitUnderTest.append("a");
+            var iterator = unitUnderTest.iterator();
+            var result = [];
+            while(iterator.hasNext()){
+                result.push(iterator.next().content);
+            }
+            expect(result).toEqual(["a"]);
+        });        
         
         
     });
--- src/List.js
+++ src/List.js
@@ -1,29 +1,62 @@
-define([], function() {
+define([], function () {
 
-    function List(){
-        
+    function Iterator(head) {
+        var current = head;
+
+        this.next = function () {
+            var result = current;
+            current = current.next;
+            return result;
+        };
+
+        this.hasNext = function () {
+            return current !== undefined;
+        };
+    }
+    ;
+
+    function List() {
+
         this.head = undefined;
         var that = this;
-        
-        this.append = function(content){
+
+        this.append = function (content) {
             var node = {
-                next:that.head,
-                prev:undefined,
-                content:content
+                next: that.head,
+                prev: undefined,
+                content: content
             };
-            if(that.head){
+            if (that.head) {
                 that.head.prev = node;
             }
             that.head = node;
-            
+
         };
-        
-        this.delete = function(node){
-            
+
+        this.delete = function (node) {
+            if (node.prev === undefined) {//remove head of list
+                if (node.next) {
+                    node.next.prev = undefined;
+                    that.head = node.next;
+                } else {
+                    that.head = undefined;
+                }
+            } else if (node.next === undefined) {//remove end of list
+                if (node.prev) {
+                    node.prev.next = undefined;
+                }
+            } else {//remove in the middle of the list
+                node.next.prev = node.prev;
+                node.prev.next = node.next;
+                node.next = undefined;
+                node.prev = undefined;
+            }
         };
-        
-        
-    }
+
+        this.iterator = function(){
+            return new Iterator(that.head);
+        } 
+    };
 
     return List;
 
